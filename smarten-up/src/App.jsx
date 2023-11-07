264e5773007c37237d0ff928b07a8cd31e171e22
Bongani Masango <bkmasango26@gmail.com>
2023-11-07 16:02:00 +0200

    nav-section

---
+++ smarten-up/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <section className="w-screen h-screen absolute bg-gradient-to-r from-purple-600 via-pink-400 to-purple-900"></section>
+  );
+};
+
+export default App;
